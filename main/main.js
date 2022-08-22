a196c4326929fb40af9ae4ecfde8d528b46c4fd2
xxxxxxxb <914401776@qq.com>
2022-08-22 13:00:09 +0800

    2022年 08月 22日 星期一 12:58:16 CST更新

--- main/main.js
+++ main/main.js
@@ -1,14 +1,13 @@
-/*let myImage = document.querySelector('img');
-myImage.onclick = function() {
+let myImage = document.querySelector('img');
+myImage.onclick = function () {
     mySrc = myImage.getAttribute('src');
-    if(mySrc === 'main/img/2233jk.jpg'){
-        myImage.setAttribute('src','main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
+    if (mySrc === 'main/img/2233jk.jpg') {
+        myImage.setAttribute('src', 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
     }
-    else if(mySrc === 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg'){
-        myImage.setAttribute('src','main/img/21308FF5-D5DD-4C64-BC1B-FAE10BE8BE15.jpeg');
+    else if (mySrc === 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg') {
+        myImage.setAttribute('src', 'main/img/21308FF5-D5DD-4C64-BC1B-FAE10BE8BE15.jpeg');
     }
-    else{
-         myImage.setAttribute('src','main/img/2233jk.jpg');
+    else {
+        myImage.setAttribute('src', 'main/img/2233jk.jpg');
     }
 }
-*/
