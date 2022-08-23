99720a545d8ba87204778fd6c7736da57580f9be
xxxxxxxb <914401776@qq.com>
2022-08-23 15:02:00 +0800

    2022年 08月 23日 星期二 15:02:00 CST更新

---
+++ mathvedio.py
@@ -0,0 +1,42 @@
+from manimlib.imports import *
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        # square.flip(RIGHT)
+        square.rotate(-3 * TAU / 8)
+        circle.set_fill(PINK, opacity=0.5)
+
+        self.play(ShowCreation(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
+
+
+class WarpSquare(Scene):
+    def construct(self):
+        square = Square()
+        self.play(ApplyPointwiseFunction(
+            lambda point: complex_to_R3(np.exp(R3_to_complex(point))),
+            square
+        ))
+        self.wait()
+
+
+class WriteStuff(Scene):
+    def construct(self):
+        example_text = TextMobject(
+            "This is a some text",
+            tex_to_color_map={"text": YELLOW}
+        )
+        example_tex = TexMobject(
+            "\\sum_{k=1}^\\infty {1 \\over k^2} = {\\pi^2 \\over 6}",
+        )
+        group = VGroup(example_text, example_tex)
+        group.arrange(DOWN)
+        group.set_width(FRAME_WIDTH - 2 * LARGE_BUFF)
+
+        self.play(Write(example_text))
+        self.play(Write(example_tex))
+        self.wait()
