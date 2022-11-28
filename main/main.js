07fe8d980b7da71ed48b820bd044c1c660b1007e
xxxxxxxb <914401776@qq.com>
2022-11-28 16:23:53 +0800

    更新,引入vue

--- main/main.js
+++ main/main.js
@@ -1,6 +1,6 @@
-let myImage = document.querySelector('img');
+/*let myImage = document.querySelector('img');
 myImage.onclick = function () {
-    mySrc = myImage.getAttribute('src');
+    let mySrc = myImage.getAttribute('src');
     if (mySrc === 'main/img/2233jk.jpg') {
         myImage.setAttribute('src', 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
     }
@@ -10,4 +10,29 @@ myImage.onclick = function () {
     else {
         myImage.setAttribute('src', 'main/img/2233jk.jpg');
     }
-}
+}*/
+const { createApp } = Vue
+
+createApp({
+    // component options
+    // declare some reactive state here.
+    data: function () {
+        return {
+            mySrc: 'main/img/2233jk.jpg'
+        }
+    },
+    methods:{
+        changeTheSrc(){
+            if (this.mySrc === 'main/img/2233jk.jpg') {
+                this.mySrc = 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg';
+            }
+            else if (this.mySrc === 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg') {
+                this.mySrc = 'main/img/21308FF5-D5DD-4C64-BC1B-FAE10BE8BE15.jpeg';
+            }
+            else {
+                this.mySrc = 'main/img/2233jk.jpg';
+            }
+        }
+    }
+}).mount('#images')
+
